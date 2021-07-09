6e25ed441ee05da6729c478b3a6050f827f7a836
Dekyi <47043635+Dekyi@users.noreply.github.com>
2021-07-09 22:03:50 +0200

    arraytest

--- testen.sh
+++ testen.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 #
 # 
-sudo dirname $(find . -maxdepth 2 | grep docker-compose.y*)
+shopt -s nullglob
+ dockerdir=(dirname "$(find . -maxdepth 2 | grep ^'docker-compose.y*')")
 
-
-
+for dir in "${dockerdir[@]}"; do
+   echo "$dir"
+done
