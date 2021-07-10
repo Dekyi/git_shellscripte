96d5d330691a4c742a3f2bf7e2d41f1538624c68
Dekyi <47043635+Dekyi@users.noreply.github.com>
2021-07-10 14:09:39 +0200

    test der gesamten funktion

--- testen.sh
+++ testen.sh
@@ -3,3 +3,11 @@
 mapfile -t dockerdir < <( dirname $(find . -maxdepth 2 | grep 'docker-compose.yml' ))
 
 printf "%s\n" "${dockerdir[@]}"
+
+for image in "${dockerdir[@]}"; do
+  echo Update von "$image"
+  sleep 1
+  cd /opt/"$image" || exit
+  docker-compose pull
+  docker-compose up -d
+ done
