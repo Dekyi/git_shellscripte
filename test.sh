0af592f0e15d8747113a2aff58253a841eaf6104
Chris <47043635+Dekyi@users.noreply.github.com>
2024-05-23 21:53:17 +0200

    Update test.sh to exclude items from the dockerdir array

--- test.sh
+++ test.sh
@@ -2,12 +2,25 @@
 
 mapfile -t dockerdir < <( dirname $(find . -maxdepth 2 | grep 'docker-compose.yml' ))
 
+#printf "%s\n" "${dockerdir[@]}"
+
+delete=(./mailcow-dockerized)
+
+for target in "${delete[@]}"; do
+  for i in "${!dockerdir[@]}"; do
+    if [[ ${dockerdir[i]} = $target ]]; then
+      unset 'dockerdir[i]'
+    fi
+  done
+done
+
 printf "%s\n" "${dockerdir[@]}"
+
 
 for image in "${dockerdir[@]}"; do
   echo Update von "$image"
   sleep 1
   cd /opt/"$image" || exit
-  docker-compose pull
-  docker-compose up -d
- done
+  docker compose pull
+  docker compose up -d
+done
